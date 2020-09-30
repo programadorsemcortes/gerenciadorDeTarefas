3e2babf2cdf84a1204cc3a398ee35f063e68993b
programadorsemcortes <programadorsemcortes@gmail.com>
2020-09-30 09:42:05 -0300

    Criando operação de instanciação

--- src/main/java/br/com/mardonio/GerenciadorDeTarefasApplication.java
+++ src/main/java/br/com/mardonio/GerenciadorDeTarefasApplication.java
@@ -1,13 +1,35 @@
 package br.com.mardonio;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GerenciadorDeTarefasApplication {
+import br.com.mardonio.domain.Tarefa;
+import br.com.mardonio.repositories.TarefaRepository;
 
+@SpringBootApplication
+public class GerenciadorDeTarefasApplication implements CommandLineRunner{
+
+	@Autowired
+	private TarefaRepository tarefaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Date d1 = new Date(System.currentTimeMillis());
+		
+		Tarefa t1 = new Tarefa(null, "Primeira Tarefa", 1, "Criar spring", d1, d1, null, null, 0);
+		Tarefa t2 = new Tarefa(null, "Segunda Tarefa", 1, "Criar BD", d1, d1, null, null, 0);
+		
+		tarefaRepository.saveAll(Arrays.asList(t1,t2));
+	}
+	
 
 }
