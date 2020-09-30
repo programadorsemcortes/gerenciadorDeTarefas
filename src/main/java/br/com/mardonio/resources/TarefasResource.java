6a4e145f96ccc5ea3c534c4cc44ac96869ecf7db
programadorsemcortes <programadorsemcortes@gmail.com>
2020-09-30 10:19:23 -0300

    :v: Paginação com parametros opcionais na requisição

--- src/main/java/br/com/mardonio/resources/TarefasResource.java
+++ src/main/java/br/com/mardonio/resources/TarefasResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -58,5 +60,15 @@ public class TarefasResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<Tarefa>> findPage(
+			@RequestParam(value = "page", defaultValue = "0" ) Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24" ) Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "titulo" ) String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC" ) String direction
+			){
+		Page<Tarefa> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
+	}
 }
